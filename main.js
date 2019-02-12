aa45e33da861a7fe3f20519a83acb9fc8486f983
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-02-12 08:01:48 -0500

    complete some implementation without completely debugging

--- main.js
+++ main.js
@@ -15,31 +15,33 @@ const dictionary = {
 		R: "press R"
 	},
 	special_abilities: {
-		I: "use item",
-		W: "use ward",
-		B: "activate recall",
-		P: "use passive",
-		A: "auto attack",
+		I: "use ITEM",
+		V: "place WARD",
+		B: "activate RECALL",
+		P: "use PASSIVE",
+		A: "auto ATTACK",
 		//AA: "auto attack",
 		//AM: "attack move",
-		F: "flash",
-		S: "summoner spell",
+		F: "FLASH",
+		S: "summoner SPELL",
 		//SS: "summoner spell",
-		M: "move"
+		M: "MOVE"
 	},
 	syntax: {//potentially multi character
-		" ": "then",
-		"*": "continuously weave in any order",
-		"/": "continuously weave in order",
-		"&": "and also",
-		"[": "start casting",
-		"]": "before end of cast",
-		"+": "activate/turn on",
-		"-": "deactivate/turn off"
+		" ": " then\n",//end
+		"*": " continuously weave in any order with ",
+		"/": " continuously weave in order with ",
+		"&": " and also ",
+		"[": " (start casting)",
+		"]": "before the end of ",
+		"+": " (activate/turn on)",
+		"-": " (deactivate/turn off)"
 	}
 };
 function decodeToEnglish(text) {//text is iLACS
 	let answer = "";
+	let tab_level = 0;
+	let holds = [];//abilities being held (array stack format)
 	for (let i = 0; i < text.length; ++i) {
 		if (dictionary.normal_abilities[text[i]]) {
 			//start of an ability
@@ -51,24 +53,43 @@ function decodeToEnglish(text) {//text is iLACS
 		}
 		else if (dictionary.syntax[text[i]]) {
 			//start of syntax
-			answer += dictionary.syntax[text[i]];
+			if (text[i] === "[") {
+				++tab_level;
+				let last_space_index = i;
+				for (; last_space_index > 0; --last_space_index) {
+					if (text[last_space_index] === " ") {
+						++last_space_index;
+						break;
+					}
+				}
+				holds.push(text.substring(last_space_index, i));
+				text = text.substring(0, i + 1) + " " + text.substring(i + 1);
+			}
+			if (text[i] === "]") --tab_level;
+			if (text[i] === " ") answer += dictionary.syntax[text[i]] + multiplyString("\t", tab_level);
+			else if (text[i] === "]") answer += "\n" + multiplyString("\t", tab_level) + dictionary.syntax[text[i]] + holds.pop() + ".";
+			else answer += dictionary.syntax[text[i]];
 		}
 		else if (text[i] === "<") {//beginning of duration
 			let hold = true;//set to true if not a delay
 			if (i - 1 < 0 || text[i - 1] == " ") hold = false;//is a delay
-			answer += decodeDuration(text.substring(i, text.indexOf(">", i) + 1));
+			answer += multiplyString("\t", tab_level) + " and " + decodeDuration(text.substring(i, text.indexOf(">", i) + 1));
 			i = text.indexOf(">", i);
 		}
 		else if (text[i] === "(") {//beginning of a time duration
-			answer += decodeTimeMarker(text.substring(i, text.indexOf(")", i) + 1));
+			answer += multiplyString("\t", tab_level) + " and " + decodeTimeMarker(text.substring(i, text.indexOf(")", i) + 1));
 			i = text.indexOf(")", i);
 		}
-		else answer += text[i];
+		else {
+			answer += text[i];
+		}
+		if (tab_level < 0) throw new Error("misplaced square brackets");
 	}
+	if (tab_level !== 0) throw new Error("misplaced square brackets");
 	return answer;
 }
 function decodeDuration(text, hold) {
-	if (text == "<exp>") return "hold and expire";
+	if (text == "<exp>") return "hold until it expires";
 	else if (text == "<max>") return "hold for maximum duration";
 	else if (text == "<min>") return "hold for minimum duration";
 	else if (text == "<>") return "hold for any duration";
@@ -89,4 +110,29 @@ function decodeTimeMarker(text) {
 		else return "at " + duration + " seconds";
 	}
 }
-console.log(decodeToEnglish("Q1 Q2 E[Q3[F A E[Q1[R]]]]"));
+function multiplyString(text, num) {
+	let answer = "";
+	for (let i = 0; i < num; ++i) answer += text;
+	return answer;
+}
+//console.log(decodeToEnglish("Q1 Q2 E[Q3[F A E[Q1[R]]]]"));
+console.log("airblade yasuo\n" + decodeToEnglish("Q1 Q2 Q3[E[Q1[R]]] Q2"));
+console.log("\n\n\n");
+console.log("shen flash taunt\n" + decodeToEnglish("E[F]"));
+console.log("\n\n\n");
+console.log("keyblade yasuo\n" + decodeToEnglish("Q1 Q2 E[Q3[F A E[Q1[R]]]]"));
+console.log("\n\n\n");
+console.log("keyblade yasuo v2\n" + decodeToEnglish("Q1 Q2 E[Q3[F]] A E[Q1[R]]"));
+console.log("\n\n\n");
+console.log("vi Q\n" + decodeToEnglish("Q<> Q<1>[V] Q<1.5>[ V] Q<max>[V ] Q<min>[ V ] Q<exp>"));
+
+/*
+console.log("\n\n\n");
+console.log(decodeToEnglish("W[Q] E"));
+console.log("\n\n\n");
+console.log(decodeToEnglish("W+ Q E/A W-"));
+console.log("\n\n\n");
+console.log(decodeToEnglish("Q1 Q2 W[Q3]"));
+console.log("\n\n\n");
+console.log(decodeToEnglish("A1[Q1] A2[Q2] A3[Q3]"));
+*/
